533b523ea0146e94b49e710c6837a2f45dffcdc3
Darshan <rl.darshan01@gmail.com>
2024-11-20 15:00:43 +0530

    fix

---
+++ app/firebaseConfig.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdUd6j_UJcIPJKUlODxJrBgKeninMWopE",
+    authDomain: "myangularfirebase-74aff.firebaseapp.com",
+    projectId: "myangularfirebase-74aff",
+    storageBucket: "myangularfirebase-74aff.appspot.com",
+    messagingSenderId: "431015899903",
+    appId: "1:431015899903:web:01fe0314456e33ec"
+};
+
+
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
+
+// Example: Initialize Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
